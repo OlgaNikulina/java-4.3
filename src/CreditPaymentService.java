1b14ac740242466a7eb2a3e8e8f73c8c1423ef68
OlgaNikulina <58890284+OlgaNikulina@users.noreply.github.com>
2020-02-27 14:07:52 +0500

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public int calculate(int time, double percent, int credit) {
-        int payment = (int) (credit * (percent/12) / (1 - 1 / Math.pow((percent/12) + 1, time*12)));
+        int payment = (int) (credit * (percent / 12) / (1 - 1 / Math.pow((percent / 12) + 1, time * 12)));
         return payment;
     }
 }
